7ff7766b32d8551b239953c5adaddf9ac0a5f6ad
Fahad <82506574+fahad-safcsp@users.noreply.github.com>
2021-04-23 17:51:46 +0300

    add: prepared the test case

--- JSONParser/Program.cs
+++ JSONParser/Program.cs
@@ -6,7 +6,18 @@ namespace JSONParser
     {
         static void Main(string[] args)
         {
-            
+            string jsonString = "{\"name\":\"mohammed\"}";
+            Input input = new Input(jsonString);
+            Tokenizer t = new Tokenizer(input, new Tokenizable[] { 
+                // handlers ...
+            });
+
+            Token token = t.tokenize();
+            while (token != null)
+            {
+                Console.WriteLine(token.Value);
+                token = t.tokenize();
+            }
         }
     }
 }
