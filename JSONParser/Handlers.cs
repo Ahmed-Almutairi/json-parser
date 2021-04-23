7931b90191d92c9b4978351fee866d601138da9d
Fahad <82506574+fahad-safcsp@users.noreply.github.com>
2021-04-23 17:38:33 +0300

    add: handlers

---
+++ JSONParser/Handlers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONParser
+{
+    public class IdTokenizer : Tokenizable
+    {
+        private List<string> keywords;
+        public IdTokenizer(List<string> keywords)
+        {
+            this.keywords = keywords;
+        }
+        public override bool tokenizable(Tokenizer t)
+        {
+            char currentCharacter = t.input.peek();
+            //Console.WriteLine(currentCharacter);
+            return Char.IsLetter(currentCharacter) || currentCharacter == '_';
+        }
+        static bool isId(Input input)
+        {
+            char currentCharacter = input.peek();
+            return Char.IsLetterOrDigit(currentCharacter) || currentCharacter == '_';
+        }
+        public override Token tokenize(Tokenizer t)
+        {
+            return new Token(t.input.Position, t.input.LineNumber,
+                "identifier", t.input.loop(isId));
+        }
+    }
+    public class NumberTokenizer : Tokenizable
+    {
+        public override bool tokenizable(Tokenizer t)
+        {
+            return Char.IsDigit(t.input.peek());
+        }
+        static bool isDigit(Input input)
+        {
+            return Char.IsDigit(input.peek());
+        }
+        public override Token tokenize(Tokenizer t)
+        {
+            return new Token(t.input.Position, t.input.LineNumber,
+                "number", t.input.loop(isDigit));
+        }
+    }
+    public class WhiteSpaceTokenizer : Tokenizable
+    {
+        public override bool tokenizable(Tokenizer t)
+        {
+            return Char.IsWhiteSpace(t.input.peek());
+        }
+        static bool isWhiteSpace(Input input)
+        {
+            return Char.IsWhiteSpace(input.peek());
+        }
+        public override Token tokenize(Tokenizer t)
+        {
+            return new Token(t.input.Position, t.input.LineNumber,
+                "whitespace", t.input.loop(isWhiteSpace));
+        }
+    }
+}
