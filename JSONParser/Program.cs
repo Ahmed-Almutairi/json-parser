ca0e7a8d54dd1fd46a05f11b4d15e25b2ae3b25b
Ahmed <dev@test.com>
2021-04-24 22:47:21 +0300

    all handlers ready

--- JSONParser/Program.cs
+++ JSONParser/Program.cs
@@ -225,44 +225,44 @@ namespace JSONParser
 
         static void Main(string[] args)
         {
-            //string jsonString = "{10.0199999999994e-2222222225 \"name\":\"mohammed\"   \"hi\"  }";
-            //Input input = new Input(jsonString);
-            //Tokenizer t = new Tokenizer(input, new Tokenizable[] {
-            //    new NumberTokenizer(),
-            //     new braOpenOpejct(),
-            //    new WhiteSpaceTokenizer(),
-            //    new colon(),
-            //    new StringTokenizer(),
-            //    new braCloseOpejct()
+            string jsonString = "{-0000e-0 \"name\":\"mohammed\"   \"hi\"  }";
+            Input input = new Input(jsonString);
+            Tokenizer t = new Tokenizer(input, new Tokenizable[] {
+                new NumberTokenizer(),
+                 new OpenBraceTokenizer(),
+                new WhiteSpaceTokenizer(),
+                new colonTokenizer(),
+                new StringTokenizer(),
+                new CloseBraceTokenizer()
 
-            //});
+            });
 
-            //Token token = t.tokenize();
-            //Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
-            //while (token != null)
-            //{
-            //    Console.WriteLine(token.Value + "  " + token.Type);
-            //    token = t.tokenize();
-            //}
+            Token token = t.tokenize();
+            Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+            while (token != null)
+            {
+                Console.WriteLine(token.Value + "  " + token.Type);
+                token = t.tokenize();
+            }
 
-            //Console.WriteLine("---------------------------");
-            //foreach (Token tk in t.all())
-            //{
-            //    Console.WriteLine(tk.Value + "  " + tk.Type);
-            //}
+            Console.WriteLine("---------------------------");
+            foreach (Token tk in t.all())
+            {
+                Console.WriteLine(tk.Value + "  " + tk.Type);
+            }
 
 
-            /*
-             * Test wiht tokens list
-             =========================================================*/
-            var t = TestCase();
+            ///*
+            // * Test wiht tokens list
+            // =========================================================*/
+            //var t = TestCase();
 
-            Parser p = new Parser();
+            //Parser p = new Parser();
 
-            JsonObject jo = p.Parse(t);
+            //JsonObject jo = p.Parse(t);
 
-            //print
-            PrintObject(jo);
+            ////print
+            //PrintObject(jo);
 
             //Console.WriteLine("\n\n\n\n=================\n\n\n\n");
 
