130f9c8dd85cde09d183b49b07900359bac473d7
fahad-safcsp <82506574+fahad-safcsp@users.noreply.github.com>
2021-04-24 12:27:20 +0300

    Impl parser (#3)
    * rename: JSONParser to Parser
    
    * impl parser class and test case
    
    * add: new parser method to handle json string

---
+++ JSONParser/Parser.cs
@@ -0,0 +1,201 @@
+﻿using JSONParser.JSONParser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONParser
+{
+    public class Parser
+    {
+        protected string KeyString = "string";
+        protected string KeyNumber = "number";
+        protected string KeyBool = "bool";
+        protected string KeyNull = "null";
+        //protected string KeyObject = "object";
+        //protected string KeyArray = "array";
+        protected string KeyOpenBracket = "open-bracket";
+        protected string KeyCloseBracket = "close-bracket";
+        protected string KeyOpenSquBra = "open-squ-bra";
+        protected string KeyCloseSquBra = "close-squ-bra";
+        protected string KeyColon = "colon";
+        protected string KeyComma = "comma";
+        protected string KeyWhitespace = "whitespace";
+
+        public JsonObject Parse(string jsonString)
+        {
+            throw new NotImplementedException();
+
+            //Input input = new Input(jsonString);
+            //Tokenizer t = new Tokenizer(input, new Tokenizable[] {
+            //    // handler
+            //});
+
+            //return Parse(t.all());
+        }
+
+        public JsonObject Parse(List<Token> tokens)
+        {
+            //remove whitespaces ?
+            RemoveWhitespaces(ref tokens);
+
+            var o = ParseObject(tokens);
+
+            if (tokens.Count > 0) throw new Exception("Invalid json string"); // there may be a } in the middle of the string that will case the parsing process to stop!
+
+            return o;
+        }
+
+        protected void RemoveWhitespaces(ref List<Token> tokens)
+        {
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (tokens[i].Type == KeyWhitespace)
+                {
+                    tokens.RemoveAt(i);
+                    i = -1;
+                }
+            }
+        }
+
+        protected JsonObject ParseObject(List<Token> t)
+        {
+            JsonObject obj = new JsonObject();
+            List<JsonKeyValue> pairs = new List<JsonKeyValue>();
+
+            //..
+            // at least two chars
+            if (t.Count < 2 || t[0].Type != KeyOpenBracket) throw new Exception("Missing {");
+            t.RemoveAt(0); // remove {
+
+            while (t.Count > 0/* && t[0].Type != KeyCloseBracket*/)
+            {
+                if (t[0].Type == KeyCloseBracket)
+                {
+                    //t.RemoveAt(0);
+                    break;
+                }
+
+                pairs.Add(ParseKeyValue(t));
+
+                // has more key value pairs?
+                //if (t.Count == 0) throw new Exception("Expecting }");
+                //if (t[0].Type == KeyCloseBracket)
+                //{
+                //    t.RemoveAt(0);
+                //    break;
+                //}
+                if (t.Count > 0 && (t[0].Type != KeyComma && t[0].Type != KeyCloseBracket))
+                    throw new Exception("Eexpecting (,) or (})");
+
+                if (t[0].Type == KeyComma && (t.Count < 2 || t[1].Type == KeyCloseBracket))
+                    throw new Exception("Unexpected ,");
+
+                if (t[0].Type == KeyComma)
+                    t.RemoveAt(0);
+            }
+
+            //..
+            if (t.Count == 0 || t[0].Type != KeyCloseBracket) throw new Exception("Missing }");
+            t.RemoveAt(0); // remove }
+
+            // return 
+            obj.Value = pairs;
+
+            return obj;
+        }
+
+        protected JsonKeyValue ParseKeyValue(List<Token> t)
+        {
+            JsonKeyValue keyValue = new JsonKeyValue();
+
+            //.. key
+            if (t.Count == 0 || t[0].Type != KeyString) throw new Exception("Expecting string value");
+            keyValue.Key = t[0].Value;
+            t.RemoveAt(0);
+
+            //.. :
+            if (t.Count == 0 || t[0].Type != KeyColon) throw new Exception("Expecting :");
+            t.RemoveAt(0);
+
+            //.. value
+            if (t.Count == 0) throw new Exception("Expecting json value");
+
+            //.. value type
+            if (t[0].Type == KeyString)
+            { keyValue.Value = new JsonString() { Value = t[0].Value }; t.RemoveAt(0); }
+
+            else if (t[0].Type == KeyNumber)
+            { keyValue.Value = new JsonNumber() { Value = double.Parse(t[0].Value) }; t.RemoveAt(0); }
+
+            else if (t[0].Type == KeyBool)
+            { keyValue.Value = new JsonBool() { Value = (t[0].Value == "true" ? true : false) }; t.RemoveAt(0); }
+
+            else if (t[0].Type == KeyNull)
+            { keyValue.Value = new JsonNull(); t.RemoveAt(0); }
+
+            else if (t[0].Type == /*KeyObject*/KeyOpenBracket)
+                keyValue.Value = ParseObject(t);
+
+            else if (t[0].Type == /*KeyArray*/KeyOpenSquBra)
+                keyValue.Value = ParseArray(t);
+
+            else
+                throw new Exception("Unknown JSON value type");
+
+            return keyValue;
+        }
+
+        protected JsonArray ParseArray(List<Token> t)
+        {
+            List<JsonValue> a = new List<JsonValue>();
+            JsonArray ja = new JsonArray();
+
+            //..
+            if (t.Count == 0 || t[0].Type != KeyOpenSquBra) throw new Exception("Expecting [");
+            t.RemoveAt(0);
+
+            while (t.Count > 0)
+            {
+                if (t[0].Type == KeyCloseSquBra)
+                {
+                    break;
+                }
+
+                //.. value type
+                if (t[0].Type == KeyString)
+                { a.Add(new JsonString() { Value = t[0].Value }); t.RemoveAt(0); }
+
+                else if (t[0].Type == KeyNumber)
+                { a.Add(new JsonNumber() { Value = double.Parse(t[0].Value) }); t.RemoveAt(0); }
+
+                else if (t[0].Type == KeyBool)
+                { a.Add(new JsonBool() { Value = (t[0].Value == "true" ? true : false) }); t.RemoveAt(0); }
+
+                else if (t[0].Type == KeyNull)
+                { a.Add(new JsonNull()); t.RemoveAt(0); }
+
+                else if (t[0].Type == /*KeyObject*/KeyOpenBracket)
+                    a.Add(ParseObject(t));
+
+                else if (t[0].Type == /*KeyArray*/KeyOpenSquBra)
+                    a.Add(ParseArray(t));
+
+                else
+                    throw new Exception("Unknown JSON value type");
+
+                //remove ,
+                if (t.Count == 0) throw new Exception("Expectig ]");
+                if (t[0].Type == KeyComma) t.RemoveAt(0); // remove ,
+            }
+
+            //..
+            if (t.Count == 0 || t[0].Type != KeyCloseSquBra) throw new Exception("Missing ]");
+            t.RemoveAt(0); // remove }
+
+            ja.Value = a.ToArray();
+
+            return ja;
+        }
+
+    }
+}
--- JSONParser/Program.cs
+++ JSONParser/Program.cs
@@ -1,22 +1,241 @@
-﻿using System;
+﻿using JSONParser.JSONParser;
+using System;
+using System.Collections.Generic;
 
 namespace JSONParser
 {
     class Program
     {
+        static List<Token> TestCase()
+        {
+            List<Token> t = new List<Token>();
+
+            //whitespace
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+
+            t.Add(new Token(0, 0, "open-bracket", "{"));
+
+            //whitespace
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+
+            //string value
+            t.Add(new Token(0, 0, "string", "name"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "string", "mohammed"));
+
+            //whitespace
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //number value
+            t.Add(new Token(0, 0, "string", "num_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "number", "51881.79"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //whitespace
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+
+            //number value
+            t.Add(new Token(0, 0, "string", "num_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "number", "51881.79"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //bool true
+            t.Add(new Token(0, 0, "string", "bool_true"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "bool", "true"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //bool false
+            t.Add(new Token(0, 0, "string", "bool_false"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "bool", "false"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //null
+            t.Add(new Token(0, 0, "string", "null_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "null", "null"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //object
+            t.Add(new Token(0, 0, "string", "object_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "open-bracket", "{"));
+
+            //string value
+            t.Add(new Token(0, 0, "string", "name"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "string", "mohammed"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //number value
+            t.Add(new Token(0, 0, "string", "num_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "number", "51881.79"));
+
+
+            t.Add(new Token(0, 0, "close-bracket", "}"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //object-empty
+            t.Add(new Token(0, 0, "string", "object_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "open-bracket", "{"));
+
+            t.Add(new Token(0, 0, "close-bracket", "}"));
+
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //array
+            t.Add(new Token(0, 0, "string", "array_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "open-squ-bra", "["));
+
+            //string value
+            //t.Add(new Token(0, 0, "string", "name"));
+            //t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "string", "mohammed"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            // empty object
+            t.Add(new Token(0, 0, "open-bracket", "{"));
+            t.Add(new Token(0, 0, "close-bracket", "}"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //number value
+            //t.Add(new Token(0, 0, "string", "num_value"));
+            //t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "number", "51881.79"));
+
+
+            t.Add(new Token(0, 0, "close-squ-bra", "]"));
+
+            //sep
+            t.Add(new Token(0, 0, "comma", ","));
+
+            //array-empty
+            t.Add(new Token(0, 0, "string", "array_value"));
+            t.Add(new Token(0, 0, "colon", ":"));
+            t.Add(new Token(0, 0, "open-squ-bra", "["));
+            t.Add(new Token(0, 0, "close-squ-bra", "]"));
+
+            //sep
+            //t.Add(new Token(0, 0, "comma", ","));
+
+            //..
+            t.Add(new Token(0, 0, "close-bracket", "}"));
+
+
+            //whitespace
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+            t.Add(new Token(0, 0, "whitespace", " "));
+
+            return t;
+        }
+
+        static void PrintObject(JsonObject o)
+        {
+            foreach (var item in o.Value)
+            {
+                if (item.Value is JsonString) Console.WriteLine($"{item.Key} : {((JsonString)item.Value).Value}");
+                else if (item.Value is JsonNumber) Console.WriteLine($"{item.Key} : {((JsonNumber)item.Value).Value}");
+                else if (item.Value is JsonBool) Console.WriteLine($"{item.Key} : {((JsonBool)item.Value).Value}");
+                else if (item.Value is JsonNull) Console.WriteLine($"{item.Key} : {((JsonNull)item.Value).Value}"); //change to string "null" ?
+
+                else if (item.Value is JsonObject)
+                {
+                    //PrintObjectValues(item.Value);
+                    Console.WriteLine($"[Object] Key = {item.Key}");
+                    PrintObject((JsonObject)item.Value);
+                }
+                else if (item.Value is JsonArray)
+                {
+                    //PrintObjectValues(item.Value);
+                    Console.WriteLine($"[Array] Key = {item.Key}");
+                    PrintArray((JsonArray)item.Value);
+                }
+                else throw new Exception("Unknown JsonValue type");
+
+            }
+        }
+
+        static void PrintArray(JsonArray a)
+        {
+            for (int i = 0; i < a.Value.Length; i++)
+            {
+                if (a.Value[i] is JsonString) Console.WriteLine($"{((JsonString)a.Value[i]).Value}");
+                else if (a.Value[i] is JsonNumber) Console.WriteLine($"{((JsonNumber)a.Value[i]).Value}");
+                else if (a.Value[i] is JsonBool) Console.WriteLine($"{((JsonBool)a.Value[i]).Value}");
+                else if (a.Value[i] is JsonNull) Console.WriteLine($"{((JsonNull)a.Value[i]).Value}");// print "null" instead?
+
+                else if (a.Value[i] is JsonObject)
+                {
+                    //PrintObjectValues(item.Value);
+                    Console.WriteLine($"[Object]");
+                    PrintObject((JsonObject)a.Value[i]);
+                }
+                else if (a.Value[i] is JsonArray)
+                {
+                    //PrintObjectValues(item.Value);
+                    Console.WriteLine($"[Array]");
+                    PrintArray((JsonArray)a.Value[i]);
+                }
+                else throw new Exception("Unknown JsonValue type");
+            }
+        }
+
         static void Main(string[] args)
         {
-            string jsonString = "{10.0199999999994e-2222222225 \"name\":\"mohammed\"   \"hi\"  }";
-            Input input = new Input(jsonString);
-            Tokenizer t = new Tokenizer(input, new Tokenizable[] {
-                new NumberTokenizer(),
-                 new braOpenOpejct(),
-                new WhiteSpaceTokenizer(),
-                new colon(),
-                new StringTokenizer(),
-                new braCloseOpejct()
+            //string jsonString = "{10.0199999999994e-2222222225 \"name\":\"mohammed\"   \"hi\"  }";
+            //Input input = new Input(jsonString);
+            //Tokenizer t = new Tokenizer(input, new Tokenizable[] {
+            //    new NumberTokenizer(),
+            //     new braOpenOpejct(),
+            //    new WhiteSpaceTokenizer(),
+            //    new colon(),
+            //    new StringTokenizer(),
+            //    new braCloseOpejct()
 
-            });
+            //});
 
             //Token token = t.tokenize();
             //Console.WriteLine(); Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
@@ -26,11 +245,39 @@ namespace JSONParser
             //    token = t.tokenize();
             //}
 
-            Console.WriteLine("---------------------------");
-            foreach (Token tk in t.all())
-            {
-                Console.WriteLine(tk.Value + "  " + tk.Type);
-            }
+            //Console.WriteLine("---------------------------");
+            //foreach (Token tk in t.all())
+            //{
+            //    Console.WriteLine(tk.Value + "  " + tk.Type);
+            //}
+
+
+            /*
+             * Test wiht tokens list
+             =========================================================*/
+            var t = TestCase();
+
+            Parser p = new Parser();
+
+            JsonObject jo = p.Parse(t);
+
+            //print
+            PrintObject(jo);
+
+            //Console.WriteLine("\n\n\n\n=================\n\n\n\n");
+
+            /*
+             * Test with json string
+             * ==========================================================*/
+            //string jsonString = "{\"name\":\"fahad\",\"bool_true\":true,\"bool_false\":false,\"null_value\":null,\"num_value\":95095,\"array_value\":[23,\"Hello there\",true,false,null],\"obj_key\":{\"i_am_key_in_sub_obj\":\"i_am_value_in_sub_object\"}}";
+
+            //Parser p2 = new Parser();
+
+            //JsonObject jo2 = p2.Parse(jsonString);
+
+            ////print
+            //PrintObject(jo2);
+
         }
     }
 }
